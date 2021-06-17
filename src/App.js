b85b46455b0abb27083a787e1e88a67182be3f3e
Knorane Eaydon <shikharpathak96@gmail.com>
2021-06-17 23:14:28 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Welcome from './Welcome'
+import Home from './Home'
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Welcome}/>
+        <Route exact path="/home" component={Home}/>
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error} />
+      </Switch>
+      <br />
+    </>
   );
 }
 
